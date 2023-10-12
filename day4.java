0ed710a3bd51998ea79626b3ee6dd957e74d3610
Nuraliah0611 <100122477+Nuraliah0611@users.noreply.github.com>
2023-10-12 19:41:18 +0800

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class day4 {
+    public static void main(String[] args) {
+        String strA = "Dalam hidup kamu akan bertemu dua jenis orang, orang yang membangunmu dan orang yang menjatuhkanmu. Namun pada akhirnya kamu akan berterima kasih pada mereka berdua";
+        String[] arrStrB = strA.split("\\s");
+        //System.out.println("Array.toString(arrStrB);
+        for (String s : arrStrB){
+            System.out.println(s);
+            
+        //Operasi split string digunakan untuk memecah sebuah string(split) menjadi array string berdasarkan pemisah (delimiter) berupa simbol tertentu
+        //noGPT but ModulPBO 2023 
+        }
+        
+    }
+    
+}
