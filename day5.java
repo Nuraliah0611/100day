694190e646a016d3404ec31c2cd95045d2805b2a
Nuraliah0611 <100122477+Nuraliah0611@users.noreply.github.com>
2023-10-13 21:57:42 +0800

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,11 @@
+package day5;
+
+public class day5 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            System.out.println("Informatic Study Club");
+            
+        }
+    }
+ 
+}
