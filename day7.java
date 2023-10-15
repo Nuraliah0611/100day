c992141cad8ffb52eb01cfa7dbc6f9e5e71f620d
Nuraliah0611 <100122477+Nuraliah0611@users.noreply.github.com>
2023-10-15 15:17:33 +0800

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,16 @@
+
+package day5;
+
+public class day7 {
+    public static void main(String[] args) {
+        int [] angka = { 6,27,23,24,4 };
+        int i = 0;
+        do {
+            System.out.println("angka :" + angka [i]);
+            i++;
+        }while ( i < angka.length);
+        
+        // contoh program do while
+        }
+    }
+
