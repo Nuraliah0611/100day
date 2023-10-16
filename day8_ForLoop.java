72f38c59f40ca011a3524097f97629472024b636
Nuraliah0611 <100122477+Nuraliah0611@users.noreply.github.com>
2023-10-16 18:39:19 +0800

    Add files via upload

---
+++ day8_ForLoop.java
@@ -0,0 +1,12 @@
+package day5;
+
+public class day8_ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 500; i++ ) {  
+            System.out.println("iterasi ke - "+ i);
+    
+        }
+        // for loop digunakan untuk mengeksekusi serangkaian kode berulangkali jika kondisi tertentu dinilai benar.
+    }
+    }
+
