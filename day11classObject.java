228e3ce1672fbac63c8b7f1404f68ae33f8500db
Nuraliah0611 <100122477+Nuraliah0611@users.noreply.github.com>
2023-10-19 21:01:09 +0800

    Add files via upload

---
+++ day11classObject.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class day11classObject {
+    String nama;
+    String warna;
+    int jumlahBuah;
+    double berat;
+    //method
+    void cetak (){
+        System.out.println("nama :" + nama);
+        System.out.println("warna : "+ warna);
+        System.out.println("jumlahBuah : " + jumlahBuah);
+        System.out.println("berat : "+ berat);
+    }
+}
---
+++ day11classmain.java
@@ -0,0 +1,12 @@
+package day5;
+
+public class day11classmain {
+    public static void main(String[] args) {
+        day11classObject lia = new day11classObject();
+        lia.nama = "Anggur";
+        lia.warna = "purple";
+        lia.jumlahBuah = 20;
+        lia.berat = 3.4;
+        lia.cetak();
+    }
+}
