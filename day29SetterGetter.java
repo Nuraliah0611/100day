c4f7220b1f62235027033e84c81bb0e3b4dbfc05
Nuraliah0611 <100122477+Nuraliah0611@users.noreply.github.com>
2023-11-06 23:36:00 +0800

    Add files via upload

---
+++ day29SetterGetter.java
@@ -0,0 +1,34 @@
+
+package day5;
+
+public class day29SetterGetter {
+
+    private String nama;
+    private String warna;
+    private int berbiji;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public int getBerbiji() {
+        return berbiji;
+    }
+
+    public void setBerbiji(int berbiji) {
+        this.berbiji = berbiji;
+    }
+}
+
