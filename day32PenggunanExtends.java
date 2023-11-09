83253ef108192292116de13802501a1a54b1c373
Nuraliah0611 <100122477+Nuraliah0611@users.noreply.github.com>
2023-11-09 23:16:31 +0800

    Add files via upload

---
+++ day32PenggunanExtends.java
@@ -0,0 +1,149 @@
+
+package day5;
+
+public class day32PenggunanExtends {
+    class BangunDatar{
+
+    //2
+    class PersegiPanjang extends BangunDatar{
+        private double luas;
+    
+        public PersegiPanjang(double luas){
+            this.luas = luas;
+        }
+    }
+    class PersegiPanjang01 extends BangunDatar{
+        private double luas;
+    
+        public PersegiPanjang01(double luas){
+            this.luas = luas;
+        }
+    }
+    
+    //3
+    class Persegi extends BangunDatar{
+        private double sisi;
+    
+        public Persegi(double sisi){
+            this.sisi = sisi;
+        }
+    }
+
+    class Persegi1 extends BangunDatar{
+        private double sisi;
+    
+        public Persegi1(double sisi){
+            this.sisi = sisi;
+        }
+    }
+    //4
+    class Lingkaran extends BangunDatar{
+        private double luas;
+    
+        public Lingkaran(double luas){
+            this.luas = luas;
+        }
+    }
+    class Lingkaran1 extends BangunDatar{
+        private double luas;
+    
+        public Lingkaran1(double luas){
+            this.luas = luas;
+        }
+    }
+    //5
+    class Trapesium extends BangunDatar{
+        private double alas;
+    
+        public Trapesium(double alas){
+            this.alas = alas;
+        }
+    }
+    class Trapesium1 extends BangunDatar{
+        private double alas;
+    
+        public Trapesium1(double alas){
+            this.alas = alas;
+        }
+    }
+    //6
+    public class Segitiga extends BangunDatar {
+        private double tinggi;
+    
+        public Segitiga(double tinggi){
+            this.tinggi = tinggi;
+        }
+    }
+    public class Segitiga2 extends BangunDatar {
+        private double tinggi;
+    
+        public Segitiga2(double tinggi){
+            this.tinggi = tinggi;
+        }
+    }
+
+    //7
+    public class JajarGenjang extends BangunDatar {
+        private double luas;
+    
+        public JajarGenjang(double luas){
+            this.luas = luas;
+        }
+    }
+    public class JajarGenjang1 extends BangunDatar {
+        private double luas;
+    
+        public JajarGenjang1(double luas){
+            this.luas = luas;
+        }
+    }
+
+    //8
+    public class BelahKetupat extends BangunDatar {
+        private double lebar;
+    
+        public BelahKetupat(double lebar){
+            this.lebar = lebar;
+        }
+    }
+    public class BelahKetupat extends BangunDatar {
+        private double lebar;
+    
+        public BelahKetupat(double lebar){
+            this.lebar = lebar;
+        }
+    }
+    //9
+    public class LayangLayang extends BangunDatar {
+        private double tinggi;
+    
+        public LayangLayang(double tinggi){
+            this.tinggi = tinggi;
+        }
+    }
+    public class LayangLayang1 extends BangunDatar {
+        private double tinggi;
+    
+        public LayangLayang1(double tinggi){
+            this.tinggi = tinggi;
+        }
+    }
+    //10
+    public class SegitigaSamaKaki extends BangunDatar {
+        private double tinggi;
+    
+        public SegitigaSamaKaki(double tinggi){
+            this.tinggi = tinggi;
+        }
+    }
+    public class SegitigaSamaKaki extends BangunDatar {
+        private double tinggi;
+    
+        public SegitigaSamaKaki(double tinggi){
+            this.tinggi = tinggi;
+            
+        }
+    }
+}
+}
+}
