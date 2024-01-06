4ddfcff041d97fe6b3583c3735abd89cef5c81a2
Nuraliah0611 <100122477+Nuraliah0611@users.noreply.github.com>
2024-01-06 19:46:01 +0700

    Add files via upload

---
+++ day90.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class day90 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        int tinggi = scanner.nextInt();
+
+        System.out.println("\nPola segitiga siku-siku:");
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
